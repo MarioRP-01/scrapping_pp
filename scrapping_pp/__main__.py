39f7cb779d17d10fd96012a458c721125153f605
MarioRP-01 <marlorosadron@gmail.com>
2022-12-12 23:36:46 +0100

    Add parameterized input functionality. Improved consistency
    There were two cases in which it did not function correctly:
    - Sometimes with the request an http response 503 was received.
    - Sometimes the device used for the request was mobile, so the selectors
      changed

--- scrapping_pp/__main__.py
+++ scrapping_pp/__main__.py
@@ -1,12 +1,20 @@
 # import amazon
+import sys
 import scrapping_pp.amazon.scrap_amazon as amazon
+import cloudscraper
 
 if __name__ == '__main__':
 
-    HEADERS = ({
-        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:35.0) Gecko/20100101 Firefox/35.0",
-        "Accept-Language": "es-ES"
-    })
+    arguments = sys.argv
+    
+    if (len(arguments) < 2 or len(arguments) > 3): sys.exit()
 
-    amazon.scrap_amazon(HEADERS)
+    scraper = cloudscraper.create_scraper()
+
+    if (len(arguments) == 2):
+        amazon.scrap_amazon(scraper, arguments[1])
+        sys.exit()
+
+    amazon.scrap_amazon(scraper, arguments[1], int(arguments[2]))
+
     
--- scrapping_pp/amazon/scrap_amazon.py
+++ scrapping_pp/amazon/scrap_amazon.py
@@ -1,43 +1,70 @@
+import sys
 from bs4 import BeautifulSoup
-import cloudscraper
 
 import scrapping_pp.common.viewer as viewer
+from cloudscraper import CloudScraper
 
-def scrap_amazon(headers):
+def scrap_amazon(scraper: CloudScraper, item = None, pages = 0):
 
-    ITEM = "playstation+4"
-    scraper = cloudscraper.create_scraper()
+    if (item is None and isinstance(item, str)): return
+    if (not isinstance(pages, int)): return
 
-    __general_information(scraper, ITEM)
+    pages = abs(pages)
+    response, status = __scrap(scraper, item, pages)
 
-    # links = list(map(lambda link: link.get('href'), links))
+    # TODO: Delete later
+    for (i, items), ok in zip(enumerate(response), status):
+        print("\n---\nPage ", i, " | Status ", ok, "\n---\n")
+        for item in items:
+            print(item, "\n")
 
 
-def __scrap(soup):
-    return []
+def __scrap(scraper: CloudScraper, item, pages):
 
-def __general_information(scraper, item):
-    URL = "https://www.amazon.es/s?k=" + item + "&ref=nb_sb_noss_2"
-    webpage = scraper.get(URL)
+    response = []
+    status = []
 
-    print(webpage)
+    for i in range(1, pages + 1):
+        pages_message = "page=" + str(i)
+        URL = "https://www.amazon.es/s?k=" + item + "&" + pages_message + "&ref=nb_sb_noss_2"
+
+        while (True):
+            webpage = scraper.get(URL)
+            if webpage.status_code == 200: break
+
+        status.append(webpage.status_code)
+        response.append(__general_information(webpage))
+
+    return response, status
+
+
+def __general_information(webpage):
 
     soup = BeautifulSoup(webpage.content, "lxml")
     items_data = soup.find_all("div", attrs={'data-component-type':'s-search-result'})
 
     items_data = list(map(__extract_general_information, items_data))
 
-    # TODO: Delete later
-    for item in items_data:
-        print(item)
-        print()
-
     return items_data
 
+
 def __extract_general_information(item_data: BeautifulSoup):
+
     product_title = item_data.select_one("span.a-size-base-plus")
     if product_title is not None:
         product_title = product_title.string.strip()
+    else:
+        product_title = item_data.select_one("h2.s-line-clamp-3")
+        if product_title is not None:
+            product_title = product_title.string.strip()
+
+    product_link = item_data.select_one(".s-no-outline")
+    if product_link is not None:
+        product_link = product_link.get("href")
+    else:
+        product_link = item_data.select_one("a[title=\"product-image\"]")
+        if product_link is not None:
+            product_link = product_link.get("href").strip()
 
     product_price = item_data.select_one("span.a-price-whole")
     if product_price is not None:
@@ -46,9 +73,17 @@ def __extract_general_information(item_data: BeautifulSoup):
     product_valoration = item_data.select_one("span[aria-label] span[class=\"a-size-base\"]")
     if product_valoration is not None:
         product_valoration = product_valoration.string.strip()
+    else:
+        product_valoration = item_data.select_one("i span.a-icon-alt")
+        if product_valoration is not None:
+            product_valoration = product_valoration.string.strip().split()[0]
 
     product_availability = item_data.select_one("span[aria-label] span.a-size-base.s-underline-text")
     if product_availability is not None:
         product_availability = product_availability.string.strip()[1:-1]
+    else:
+        product_availability = item_data.select_one("span[aria-label] span.puis-light-weight-text")
+        if product_availability is not None:
+            product_availability = product_availability.string.strip()[1:-1]
 
-    return [product_title, product_price, product_valoration, product_availability]
+    return [product_title, product_link, product_price, product_valoration, product_availability]
